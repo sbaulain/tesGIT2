2f4f4b298b54ab1ba9686b19a717d2cc009ee98c
Juliette <juliette.lemains@insa-rouen.fr>
2017-03-17 10:29:53 +0100

    prout prout chat

---
+++ test2/src/test2.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : test2.cpp
+// Author      : Juliettmé
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout <<"premiere modif popo";
+	return 0;
+}
